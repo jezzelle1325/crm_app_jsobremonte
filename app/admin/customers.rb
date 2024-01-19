0784e60a1a8b11ea673c5ceb67bc052bb9315989
Jezzelle <jezreelsonsobremonte@ymail.com>
2024-01-18 19:54:38 -0600

    Added active_storage_extensions.rb under config/initializers to get rid of Ransack Errors

---
+++ app/admin/customers.rb
@@ -0,0 +1,42 @@
+=begin
+
+    Name: Jezreelson Sobremonte
+    Course: WEBD-3008 (248294)
+    Instructor: Jonathan Boisvert
+    Challenge 6
+
+
+    Customers on Admin (Testing the image uploading)
+
+=end
+
+ActiveAdmin.register Customer do
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email_address
+    column :notes
+    column :image do |customer|
+      if customer.image.attached?
+        div style: "height: 50px; width: 50px; overflow: hidden; border-radius: 50%;" do
+          image_tag customer.image, style: "height: auto; width: 100%;"
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,4 +12,12 @@
 class Customer < ApplicationRecord
   has_one_attached :image
 
+  def self.ransackable_attributes(auth_object = nil)
+    ['full_name', 'phone_number', 'email_address', 'notes']
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ['image_attachment', 'image_blob']
+  end
 end
+
